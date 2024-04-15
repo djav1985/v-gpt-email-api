e4e135046a2ed05dec986bbd0f1f0bc306bc0946
Nikolai X. Shadeauxs <services@vontainment.com>
2024-04-14 20:52:42 -0400

    Update functions.py

--- app/functions.py
+++ app/functions.py
@@ -1,6 +1,5 @@
 from fastapi import HTTPException
 
-# Assuming 'accounts' is available here, either by passing it to the function or by importing
 def get_account_details(email: str, accounts):
     account_details = next((acc for acc in accounts if acc['email'] == email), None)
     if not account_details:
