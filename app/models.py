a70bc6be46e642cd97c3335700f2a55e8c4324d7
vontainment <services@vontainment.com>
2024-05-24 15:00:54 -0400

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,16 +1,14 @@
 from pydantic import BaseModel, EmailStr, Field, HttpUrl
 from typing import Union, List, Optional
 
+
 class SendEmailRequest(BaseModel):
     to_address: Union[EmailStr, List[EmailStr]] = Field(
         ..., description="The recipient's email address or list of email addresses."
     )
-    subject: str = Field(
-        ..., description="The subject of the email.", max_length=255
-    )
-    body: str = Field(
-        ..., description="The body content of the email."
-    )
+    subject: str = Field(..., description="The subject of the email.", max_length=255)
+    body: str = Field(..., description="The body content of the email.")
     file_url: Optional[Union[HttpUrl, List[HttpUrl]]] = Field(
-        None, description="The URL or list of URLs of the files to be downloaded and attached to the email."
+        None,
+        description="The URL or list of URLs of the files to be downloaded and attached to the email.",
     )
